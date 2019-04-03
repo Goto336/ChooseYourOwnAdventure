a9be163827faba1377c1f9b2ec08034d61526e06
Zeyd Alabsi <gotozeyd336@gmail.com>
2019-04-03 02:28:52 +0000

    final edit

--- js/game.js
+++ js/game.js
@@ -22,12 +22,17 @@ var game = {
 
         cave: {
             background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            music: "InFamous.mp3",
             message: "You walk through the door and see two ways to go. Do you go left or right?",
-            choices: [
+             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Go left",
+                    nextLevel: "luckyroom",
+                },
+
+                {
+                    text: "Go right",
+                    nextLevel: "nightmare",
                 },
             ]
         },
@@ -37,6 +42,117 @@ var game = {
             choices: [
                 {
                     text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        nightmare: {
+            background_image: "giphy.gif",
+            music: "Guardian Battle.mp3",
+            message: "You encounter your demon self and is chargering at you. What do you do?",
+            choices: [
+                {
+                    text: "Face your fears",
+                    nextLevel: "death1",
+                },
+
+                {
+                    text: "Dip",
+                    nextLevel: "death2",
+                },
+            ]
+        },
+        
+        death1: {
+            background_image: "gameover.jpg",
+            music: "Challenge Failed.mp3",
+            message: "You try to fight your demon self but since you have no weapon, it beats and stabs you and you die. What did you expect?",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        death2: {
+            background_image: "gameover.jpg",
+            music: "Challenge Failed.mp3",
+            message: "Before you get clapped, you decide to run. But you realize that you have nowhere to run and you still get clapped and die. Tough luck buddy.",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        luckyroom: {
+            background_image: "glow.gif",
+            music: "Uncharted Mid.mp3",
+            message: "You see an glowing object on the floor. Do you pick it up?",
+            choices: [
+                {
+                    text: "OBVIOUSLY!",
+                    nextLevel: "goodmare",
+                },
+
+                {
+                    text: "YEET",
+                    nextLevel: "death3",
+                },
+            ]
+        },
+        
+        death3: {
+            background_image: "gameover.jpg",
+            music: "Challenge Failed.mp3",
+            message: "Not wanting to risk your life by touching the object, you got stabbed from behide by a demon figure and died.",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+       goodmare: {
+            background_image: "giphy.gif",
+            music: "Guardian Battle.mp3",
+            message: "You encounter your demon self and is chargering at you. What do you do?",
+            choices: [
+                {
+                    text: "COME AT ME BRO!",
+                    nextLevel: "goodend",
+                },
+
+                {
+                    text: "Yeet the attack and dip",
+                    nextLevel: "death4",
+                },
+            ]
+        },
+        
+        death4: {
+            background_image: "gameover.jpg",
+            music: "Challenge Failed.mp3",
+            message: "Even with your glowing object, you chicken out and run. The demon catches you and kills you. Let's be honest, WHERE WERE YOU GOING TO GO???",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        goodend: {
+            background_image: "YouWin.jpg",
+            music: "Uncharted End.mp3",
+            message: "Using the glowing object you picked up, a fire awakens inside you and you manage to match your demon self. You daze the demon and charge a super punch. You punch the demon so hard, it turns into ashes. You then go and try to find a way out of this hell.",
+            choices: [
+                {
+                    text: "Congratulation! You live to fight another day. Start over?",
                     nextLevel: "start",
                 },
             ]
